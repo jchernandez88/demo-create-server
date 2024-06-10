dc1df9799f9b73b676f97708bbf560be45142afa
Julio Hernández <jchernandez88@users.noreply.github.com>
2024-06-10 16:57:43 -0600

    Create variables.tf

---
+++ tf-example/variables.tf
@@ -0,0 +1,12 @@
+variable "ec2_name" {
+  type = string
+}
+variable "aws_region" {
+  type = string
+}
+variable "aws_instance_type" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
